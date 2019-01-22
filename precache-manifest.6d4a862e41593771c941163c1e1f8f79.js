463b7f86a0cd58efdab4dbdfb216d7864e7c02cb
costin06 <calin735@gmail.com>
2019-01-22 19:06:08 +0200

    Updates

---
+++ precache-manifest.6d4a862e41593771c941163c1e1f8f79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "620966015bfc98da9364",
+    "url": "/ScoreBoard/static/js/runtime~main.62096601.js"
+  },
+  {
+    "revision": "62819b27e849fc87ec39",
+    "url": "/ScoreBoard/static/js/main.62819b27.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/ScoreBoard/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "62819b27e849fc87ec39",
+    "url": "/ScoreBoard/static/css/main.e23f4b7d.chunk.css"
+  },
+  {
+    "revision": "cd35ee64b6232427e72a477a98960b0e",
+    "url": "/ScoreBoard/index.html"
+  }
+];
